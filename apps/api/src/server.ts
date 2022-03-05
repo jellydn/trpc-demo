020cfe294d605a4b540cba35572c063fd0666292
Huynh Duc Dung <dunghd.it@gmail.com>
2022-03-06 00:58:48 +0800

    fix(api): support cors on fastify adapter

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -19,7 +19,36 @@ const app = Fastify({
 
 void app.register(fp(fastifyTRPCPlugin), {
   prefix: "/trpc",
-  trpcOptions: { router: appRouter, createContext },
+  trpcOptions: {
+    router: appRouter,
+    createContext,
+    responseMeta(opts: any) {
+      if (
+        opts.errors?.[0]?.code === "METHOD_NOT_SUPPORTED" &&
+        String(opts.errors?.[0]?.message ?? "").includes(
+          "Unexpected request method OPTIONS"
+        )
+      ) {
+        return {
+          status: 204,
+          headers: {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "access-control-allow-origin": "*",
+            vary: "Origin",
+          },
+        };
+      }
+
+      return {
+        // Enable CORS, refer https://github.com/trpc/trpc/issues/623#issuecomment-878639248
+        headers: {
+          "Access-Control-Allow-Headers": "Content-Type",
+          "access-control-allow-origin": "*",
+          vary: "Origin",
+        },
+      };
+    },
+  },
 });
 
 // Register your application as a normal plugin.
