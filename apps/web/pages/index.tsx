22d92a6e808b7b83463d08954e7b3ed80a1f5e92
Huynh Duc Dung <dunghd.it@gmail.com>
2022-03-05 21:05:51 +0800

    feat(apps): add no class CSS framework to web

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -2,16 +2,33 @@ import { Button } from "@productsway/ui";
 import { trpc } from "../utils/trpc";
 
 export default function Web() {
-  const { data, error, isLoading } = trpc.useQuery(["hello", { text: "web" }]);
+  const helloWithParam = trpc.useQuery([
+    "hello",
+    { text: "trpc demo with nextjs" },
+  ]);
+  const helloWithoutParam = trpc.useQuery(["hello"]);
 
   // show error if fetch issue
-  if (error) {
-    return <div>{error.message}</div>;
+  if (helloWithoutParam.error || helloWithParam.error) {
+    return (
+      <div>
+        {helloWithoutParam.error?.message || helloWithParam.error?.message}
+      </div>
+    );
   }
 
   return (
     <div>
-      {isLoading ? <div>Loading...</div> : <div>{data?.greeting}</div>}
+      {helloWithoutParam.isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <p>{helloWithoutParam.data?.greeting}</p>
+      )}
+      {helloWithParam.isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <p>{helloWithParam.data?.greeting}</p>
+      )}
       <Button name="Greet" onClick={console.warn} />
     </div>
   );
