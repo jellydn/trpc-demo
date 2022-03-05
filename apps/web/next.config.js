8719ee08fb1545e4c4cc78bbb2adf762c2b8dab8
Huynh Duc Dung <dunghd.it@gmail.com>
2022-03-05 19:11:10 +0800

    refactor(apps): change naming for common packages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,4 @@
-const withTM = require("next-transpile-modules")(["ui"]);
+const withTM = require("next-transpile-modules")(["@productsway/ui"]);
 
 module.exports = withTM({
   reactStrictMode: true,
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from "ui";
+import { Button } from "@productsway/ui";
 
 export default function Web() {
   return (
